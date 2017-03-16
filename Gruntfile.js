dc20465825c4f345282ceac91a543d34226efef2
Orlin Nedyalkov <onedyalkov@hhogdev.com>
2017-03-16 11:31:24 +0200

    Removed Foundation grid in favour of a similar custom grid.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,100 +1,100 @@
 // Define JavaScript files and their order for aggregation
 var js_files = [
-  'js_src/custom.js',
+	'js_src/custom.js',
 ];
 
 // Grunt
 module.exports = function(grunt) {
 
-  grunt.initConfig({
+	grunt.initConfig({
 
-    // jshint
-    jshint: {
-      files: [
-        'Gruntfile.js',
-        'js_src/**/*.js',
-      ],
-      options: {
-        globals: {
-          jQuery: true,
-        },
-      },
-    },
+		// jshint
+		jshint: {
+			files: [
+				'Gruntfile.js',
+				'js_src/**/*.js',
+			],
+			options: {
+				globals: {
+					jQuery: true,
+				},
+			},
+		},
 
-    // compass
-    compass: {
-      dev: {
-        options: {
-          cssDir: 'css_js',
-          sassDir: 'sass',
-          config: 'config.rb',
-        },
-      },
-    },
+		// compass
+		compass: {
+			dev: {
+				options: {
+					cssDir: 'css_js',
+					sassDir: 'sass',
+					config: 'config.rb',
+				},
+			},
+		},
 
-    // uglify
-    uglify: {
-      options: {
-        mangle: false,
-      },
-      my_target: {
-        files : {
-          'css_js/custom.js': js_files,
-        },
-      },
-    },
+		// uglify
+		uglify: {
+			options: {
+				mangle: false,
+			},
+			my_target: {
+				files : {
+					'css_js/custom.js': js_files,
+				},
+			},
+		},
 
-    // csscount
-    csscount: {
-      dev: {
-        src: [
-          'css_js/*.css',
-        ],
-        options: {
-          maxSelectors: 4000,
-          maxSelectorDepth: 5,
-          beForgiving: true
-        },
-      },
-    },
+		// csscount
+		csscount: {
+			dev: {
+				src: [
+					'css_js/*.css',
+				],
+				options: {
+					maxSelectors: 4000,
+					maxSelectorDepth: 5,
+					beForgiving: true
+				},
+			},
+		},
 
-    // watch
-    watch: {
-      set1: {
-        files: 'sass/**/*.scss',
-        tasks: ['compass'],
-      },
-      set2: {
-        files: ['<%= csscount.dev.src %>'],
-        tasks: ['csscount'],
-      },
-      set3: {
-        files: ['<%= jshint.files %>'],
-        tasks: ['jshint', 'uglify'],
-      },
-    },
+		// watch
+		watch: {
+			set1: {
+				files: 'sass/**/*.scss',
+				tasks: ['compass'],
+			},
+			set2: {
+				files: ['<%= csscount.dev.src %>'],
+				tasks: ['csscount'],
+			},
+			set3: {
+				files: ['<%= jshint.files %>'],
+				tasks: ['jshint', 'uglify'],
+			},
+		},
 
-    // notify
-    notify_hooks: {
-      options: {
-        title: "Grunt",
-        enabled: true,
-        success: true,
-        duration: 3,
-      }
-    },
+		// notify
+		notify_hooks: {
+			options: {
+				title: "Grunt",
+				enabled: true,
+				success: true,
+				duration: 3,
+			}
+		},
 
-  });
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-css-count');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-notify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-css-count');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-notify');
 
-  grunt.registerTask('default', ['compass', 'csscount', 'jshint', 'uglify']);
+	grunt.registerTask('default', ['compass', 'csscount', 'jshint', 'uglify']);
 
-  grunt.task.run('notify_hooks');
+	grunt.task.run('notify_hooks');
 
 };
