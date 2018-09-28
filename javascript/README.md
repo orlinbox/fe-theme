992d74c7a27929b1b96aa531577c28c10d5a939f
Orlin Nedyalkov <onedyalkov@hhogdev.com>
2018-09-28 15:28:42 +0300

    Bootstrap cleanup.

--- javascript/README.md
+++
@@ -1,1 +0,0 @@
-JavaScripts goes in this folder.
--- javascript/custom/custom.js
+++
@@ -1,4 +0,0 @@
-$('body').on('swipeleft', function(){ console.log('Swipe left'); });
-$('body').on('swiperight', function(){ console.log('Swipe right'); });
-
-console.log('FE theme');
